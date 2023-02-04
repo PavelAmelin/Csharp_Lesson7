9c7cc964fe654cb3ba684e3777ef3b54667988f7
Pavel <sivec_p@mail.ru>
2023-02-04 22:00:51 +0300

    Add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,21 @@
+﻿void GetArray()
+{
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число, до модуля которого будут генерироваться числа в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double[,] arr = new double[m, n];
+for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((new Random().NextDouble() * 2 - 1) * N, 1);
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }    
+}
+
+GetArray();
